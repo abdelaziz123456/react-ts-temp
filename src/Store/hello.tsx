d02018d42efdcb955a6ce807da4ed2155c8b8862
abdelaziz123456 <abdelazizmohamed1233@gmail.com>
2023-06-17 19:50:46 +0300

    add some packages

--- src/Store/hello.tsx
+++ src/Store/hello.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Dummy } from "Utiles";
-
+import "./hello.scss";
 export default function Hello() {
   return (
-    <div>
+    <div className="container">
+      <button className="btn btn-primary">helloo</button>
       <Dummy />
     </div>
   );
